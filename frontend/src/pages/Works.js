9049e7a7ea922f1f925d253622cd315e156ddd28
Zatoon.Dev 🫒 <154544758+Zatoon-Dev@users.noreply.github.com>
2024-09-09 19:07:55 +0300

    Add files via upload

---
+++ frontend/src/pages/Works.js
@@ -0,0 +1,22 @@
+import "./Works.css"
+import Card from "../components/Card"
+
+function Works(){
+    return (
+        <div class="body">  
+            <div className="card_container">
+
+                <Card image="./card_photos/darb.png" title="darb" link=""/>
+                <Card image="./card_photos/zero.jpg" title="zero os" link=""/>
+                <Card image="./card_photos/potato.jpg" title="potato" link=""/>
+                <Card image="./card_photos/default.png" title="none" link=""/>
+                <Card image="./card_photos/darb.png" title="darb" link=""/>
+                <Card image="./card_photos/zero.jpg" title="zero os" link=""/>
+                <Card image="./card_photos/potato.jpg" title="potato" link=""/>
+                <Card image="./card_photos/default.png" title="none" link=""/>
+
+            </div>
+        </div>
+    )
+}
+export default Works
