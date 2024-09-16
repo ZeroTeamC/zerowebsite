e2fd8dea1d7cd4cedc5305897b7174e5fe7ecc1b
Hesham <www.hichamhot@gmail.com>
2024-09-16 14:06:41 +0100

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,7 +8,7 @@ import { router } from './router';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router}></RouterProvider>
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
